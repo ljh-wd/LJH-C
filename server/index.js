79f11c307c739fb4ebb95cda0119ee76aa3b89f7
Luke <93337982+ljh-wd@users.noreply.github.com>
2023-02-08 15:30:02 +0000

    Set up server and DB connection

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+const connectDB = require("./DB/connectDB");
+
+const app = express();
+
+app.use(
+  cors({
+    // origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome");
+});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}...`));
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+start();
