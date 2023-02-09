5f3e437e328434983ec78507a2e56d75d3cb6c95
Luke <93337982+ljh-wd@users.noreply.github.com>
2023-02-09 19:33:05 +0000

    nothing important

---
+++ client/src/layouts/nav.tsx
@@ -0,0 +1,25 @@
+const Nav = () => {
+
+    let links = [
+        {href: '/', title: 'Home', key: Math.floor(Math.random() * 100)},
+        {href: `/shop`, title: 'Shop', key: Math.floor(Math.random() * 100)},
+        {href: `/contact`, title: 'Contact', key: Math.floor(Math.random() * 100)},
+    ]
+
+  return (
+    <header className="w-full bg-gray-900 h-[500px]">
+        <h1 className="playfair">LJH-C</h1>
+        <nav>
+            {
+                links.map(link => {
+                    return (
+                        <a href={link.href} key={link.key}>{link.title}</a>
+                    )
+                })
+            }
+        </nav>
+    </header>
+  );
+};
+
+export default Nav;
