ddd8c1c55e844c1f687b39048a73c1486b0e50fd
Luke <93337982+ljh-wd@users.noreply.github.com>
2023-02-10 18:35:40 +0000

    Added a bit more style to the page and added primaryBtn component.

--- client/src/Pages/home.tsx
+++ client/src/Pages/home.tsx
@@ -1,13 +1,15 @@
 import Nav from "../layouts/nav";
 import Footer from "../layouts/footer";
+import PrimaryBtn from "../layouts/primary-btn";
 
 const Home = () => {
   return (
     <div className="w-full h-screen flex flex-col">
       <Nav />
       <div className="h-full grid grid-rows-1 grid-cols-6">
-        <div className="col-span-6 md:col-span-2">
-          <h1>Your style is what you choose.</h1>
+        <div className="col-span-6 md:col-span-2 flex justify-center items-center pb-40 flex-col gap-10">
+          <h1 className="playfair text-3xl">Your style is what you choose.</h1>
+          <PrimaryBtn>Shop Now</PrimaryBtn>
         </div>
         <div className="landing-page bg-clip-path w-full bg-cover bg-right bg-no-repeat h-full col-span-4"></div>
       </div>
--- client/src/layouts/primary-btn.tsx
+++ client/src/layouts/primary-btn.tsx
@@ -1,1 +1,13 @@
-export {}
+interface Props {
+  children: String;
+}
+
+const PrimaryBtn = (props: Props) => {
+  return (
+    <button className="playfair bg-[#222] text-[#eee] uppercase px-7 py-3 rounded">
+      {props.children}
+    </button>
+  );
+};
+
+export default PrimaryBtn;
