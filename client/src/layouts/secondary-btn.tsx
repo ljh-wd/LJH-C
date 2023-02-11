96fb4ea7fa7dd93ecd754b21915c7223c785ecf9
Luke <93337982+ljh-wd@users.noreply.github.com>
2023-02-11 15:20:59 +0000

    Made small changes to PrimaryBtn and added SecondaryBtn component

--- client/src/layouts/secondary-btn.tsx
+++ client/src/layouts/secondary-btn.tsx
@@ -1,1 +1,13 @@
-export {}
+interface Props {
+  children: String;
+}
+
+const SecondaryBtn = (props: Props) => {
+  return (
+    <button className="playfair tracking-wider bg-transparent text-[#222] uppercase px-7 py-3 rounded hover:scale-105 transition hover:bg-[#222] hover:text-[#eee] border border-[#222] ">
+      {props.children}
+    </button>
+  );
+};
+
+export default SecondaryBtn;
