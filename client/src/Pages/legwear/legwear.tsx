16fbc60748db4371e4af41a63d00f3e8eb967a0d
Luke <93337982+ljh-wd@users.noreply.github.com>
2023-02-13 21:38:10 +0000

    Added placeholders for the other categories

---
+++ client/src/Pages/legwear/legwear.tsx
@@ -0,0 +1,39 @@
+// TODO customUseQuery() Hook.
+// TODO Make product card with all details.
+
+// import ProductCard from "../Components/productCard";
+import Nav from "../../layouts/nav";
+import Footer from "../../layouts/footer";
+import ProductCard from "../../Components/productCard";
+import useCustomQuery from "../../Hooks/customUseQuery";
+
+const Tops = () => {
+  const { data, error, isLoading } = useCustomQuery(
+    "tops",
+    "http://localhost:8000/api/tops"
+  );
+
+  return (
+    <>
+      <Nav />
+      {data && (
+        <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center gap-y-20 gap-x-14 mt-10 mb-5">
+          {error && <div>{error}</div>}
+          {isLoading && <div>Loading</div>}
+          {data.products.map((product: any) => (
+            <ProductCard
+              title={product.name}
+              imgUrl={product.imgUrl}
+              amount={product.amount}
+              key={product._id}
+              id={product._id}
+            />
+          ))}
+        </div>
+      )}
+      <Footer />
+    </>
+  );
+};
+
+export default Tops;
