a768d5299f189cd1a98e289c358f5a62f48dcae5
Luke <93337982+ljh-wd@users.noreply.github.com>
2023-02-20 20:19:03 +0000

    Forgot to delete some unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,8 @@
-// TODO Add Contact Page: FORM
 // TODO Add Basket: Server, Localstorage?
 
 import Home from "./Pages/home";
-import Categories from "./Pages/categories-page";
 import { Routes, Route } from "react-router-dom";
-import Tops from "./Pages/products/products";
 import ProductsID from "./Pages/products/productsID";
-import Jumpers from "./Pages/jumpers/jumpers";
-import JumpersId from "./Pages/jumpers/jumpersID";
-import CoatsId from "./Pages/coats/coatsID";
-import Coats from "./Pages/coats/coats";
-import Tailoring from "./Pages/tailoring/tailoring";
-import TailoringID from "./Pages/tailoring/tailoringID";
-import Gymwear from "./Pages/gymwear/gymwear";
-import GymwearID from "./Pages/gymwear/gymwearID";
-import FootwearID from "./Pages/footwear/footwearID";
-import Footwear from "./Pages/footwear/footwear";
-import Legwear from "./Pages/legwear/legwear";
-import LegwearId from "./Pages/legwear/legwearID";
 import NotFound from "./Pages/404-page";
 import Contact from "./Pages/contact";
 import Basket from "./Pages/basket";
