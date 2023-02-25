620845518bd0dc3ff8a7dc299cd9cacc3b4f3b14
Luke <93337982+ljh-wd@users.noreply.github.com>
2023-02-25 17:33:56 +0000

    cleaned up code ready to deploy

--- server/Controllers/products.js
+++ server/Controllers/products.js
@@ -2,38 +2,12 @@ const Products = require("../Models/Products");
 const { createCustomError } = require("../errors/custom-error");
 
 const getAllProducts = async (req, res) => {
-  const { name, amount, colour, numericFilters, sort } = req.query;
+  const { name, amount, sort } = req.query;
 
   const queryObject = {};
 
   if (name) queryObject.name = { $regex: name, $options: "i" };
   if (amount) queryObject.amount = amount;
-  if (colour) queryObject.colour = { $regex: colour, $options: "i" };
-
-  if (numericFilters) {
-    const operatorMap = {
-      ">": "$gt",
-      ">=": "$gte",
-      "=": "$eq",
-      "<": "$lt",
-      "<=": "$lte",
-    };
-
-    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
-
-    let filters = numericFilters.replace(
-      regEx,
-      (match) => `-${operatorMap[match]}-`
-    );
-
-    const options = ["amount"];
-    filters = filters.split(",").forEach((item) => {
-      const [field, operator, value] = item.split("-");
-      if (options.includes(field)) {
-        queryObject[field] = { [operator]: Number(value) };
-      }
-    });
-  }
 
   let result = Products.find(queryObject);
   if (sort) {
